64d9ff1f313c900f3904be70c3b97205e73f2018
board707 <dd@jwee.ru>
2023-03-22 19:41:15 +0300

    Add EEPROM library and hal_internal_flash example

---
+++ libraries/EEPROM/EEPROM.h
@@ -0,0 +1,263 @@
+/*
+  EEPROM.h - EEPROM library
+  Original Copyright (c) 2006 David A. Mellis.  All right reserved.
+  New version by Christopher Andrews 2015.
+  This copy has minor modificatons for use with Teensy, by Paul Stoffregen
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+/* This file is part of Arduino project for Winner Micro boards
+  
+     Original Arduino EEPROM.h by Paul Stoffregen
+     https://github.com/PaulStoffregen/EEPROM
+
+     Modified for w80x boards by Dmitry E. Dmitriev 
+*/
+
+#ifndef EEPROM_h
+#define EEPROM_h
+
+//#define ARDUINO_STRING_SUPPORT 
+//#define EEPROM_ADVANCED
+//#define EEPROM_END_ADDR INSIDE_FLS_END_ADDR 
+#define EEPROM_END_ADDR 0x80FFFFF
+#define EEPROM_DEFAULT_SIZE 0x0800          // 2048 bytes
+
+//#include <inttypes.h>
+#include <Arduino.h>
+//#include <wm_internal_flash.h>
+
+
+#if defined(__has_include) && __has_include(<type_traits>)
+#include <type_traits>
+#endif
+
+#if defined(ARDUINO_STRING_SUPPORT)
+#include <WString.h>    // need to include this so String is defined
+#endif
+
+
+#if defined( EEPROM_ADVANCED )
+/***
+    EERef class.
+
+    This object references an EEPROM cell.
+    Its purpose is to mimic a typical byte of RAM, however its storage is the EEPROM.
+    This class has an overhead of two bytes, similar to storing a pointer to an EEPROM cell.
+***/
+
+struct EERef{
+
+    EERef( const int index )
+        : index( index )                 {}
+
+    //Access/read members.
+    uint8_t operator*() const            { return eeprom_read_byte( (uint8_t*) index ); }
+    operator const uint8_t() const       { return **this; }
+
+    //Assignment/write members.
+    EERef &operator=( const EERef &ref ) { return *this = *ref; }
+    EERef &operator=( uint8_t in )       { return eeprom_write_byte( (uint8_t*) index, in ), *this;  }
+    EERef &operator +=( uint8_t in )     { return *this = **this + in; }
+    EERef &operator -=( uint8_t in )     { return *this = **this - in; }
+    EERef &operator *=( uint8_t in )     { return *this = **this * in; }
+    EERef &operator /=( uint8_t in )     { return *this = **this / in; }
+    EERef &operator ^=( uint8_t in )     { return *this = **this ^ in; }
+    EERef &operator %=( uint8_t in )     { return *this = **this % in; }
+    EERef &operator &=( uint8_t in )     { return *this = **this & in; }
+    EERef &operator |=( uint8_t in )     { return *this = **this | in; }
+    EERef &operator <<=( uint8_t in )    { return *this = **this << in; }
+    EERef &operator >>=( uint8_t in )    { return *this = **this >> in; }
+
+    EERef &update( uint8_t in )          { return  in != *this ? *this = in : *this; }
+
+    /** Prefix increment/decrement **/
+    EERef& operator++()                  { return *this += 1; }
+    EERef& operator--()                  { return *this -= 1; }
+
+    /** Postfix increment/decrement **/
+    uint8_t operator++ (int) {
+        uint8_t ret = **this;
+        return ++(*this), ret;
+    }
+
+    uint8_t operator-- (int) {
+        uint8_t ret = **this;
+        return --(*this), ret;
+    }
+
+    int index; //Index of current EEPROM cell.
+};
+
+/***
+    EEPtr class.
+
+    This object is a bidirectional pointer to EEPROM cells represented by EERef objects.
+    Just like a normal pointer type, this can be dereferenced and repositioned using
+    increment/decrement operators.
+***/
+
+struct EEPtr{
+
+    EEPtr( const int index )
+        : index( index )                {}
+
+    operator const int() const          { return index; }
+    EEPtr &operator=( int in )          { return index = in, *this; }
+
+    //Iterator functionality.
+    bool operator!=( const EEPtr &ptr ) { return index != ptr.index; }
+    EERef operator*()                   { return index; }
+
+    /** Prefix & Postfix increment/decrement **/
+    EEPtr& operator++()                 { return ++index, *this; }
+    EEPtr& operator--()                 { return --index, *this; }
+    EEPtr operator++ (int)              { return index++; }
+    EEPtr operator-- (int)              { return index--; }
+
+    int index; //Index of current EEPROM cell.
+};
+
+
+#endif   // end of advanced section
+
+/***
+    EEPROMClass class.
+
+    This object represents the entire EEPROM space.
+    It wraps the functionality of EEPtr and EERef into a basic interface.
+    This class is also 100% backwards compatible with earlier Arduino core releases.
+***/
+
+struct EEPROMClass{
+
+
+    EEPROMClass(uint16_t size = EEPROM_DEFAULT_SIZE)                 { init(size); }
+
+    uint32_t ee_start = 0; //0x80FF7FF;
+    void init(uint16_t size)  
+       {
+        ee_start = EEPROM_END_ADDR - size;
+       }
+    
+    
+    uint8_t eeprom_read_byte( int idx ) {
+        uint8_t buf;
+        HAL_FLASH_Read(ee_start + idx, &buf, sizeof(buf));
+        return buf;
+    }
+    
+    uint8_t eeprom_write_byte( int idx, uint8_t val ) {
+        uint8_t buf = val;
+        if (HAL_OK == HAL_FLASH_Write(ee_start + idx, &buf, sizeof(buf)))  return 1;
+        return 0;
+    }
+
+    uint8_t eeprom_read_block( int idx , uint8_t* ptr, uint16_t b_size) {
+        if (HAL_OK == HAL_FLASH_Read(ee_start + idx, ptr, b_size)) return 1;
+        return 0;
+    }
+
+    uint8_t eeprom_write_block( int idx , uint8_t* ptr, uint16_t b_size) {
+        if (HAL_OK == HAL_FLASH_Write(ee_start + idx, ptr, b_size))  return 1;
+        return 0;
+    }
+    
+    
+    //Basic user access methods.
+    //int operator[]( const int idx )      { return read(idx); }
+    uint8_t read( int idx )              { return eeprom_read_byte( idx ); }
+    uint8_t write( int idx, uint8_t val )   { return eeprom_write_byte( idx, val ); }
+    uint8_t update( int idx, uint8_t val )  { if (val != eeprom_read_byte( idx )) 
+                                               return eeprom_write_byte( idx, val ); 
+                                              else return 1; }
+
+    //STL and C++11 iteration capability.
+    //EEPtr begin()                        { return 0x00; }
+    //EEPtr end()                          { return length(); } //Standards requires this to be the item after the last valid entry. The returned pointer is invalid.
+    uint16_t length()                    { return EEPROM_END_ADDR - ee_start; }
+
+    //Functionality to 'get' and 'put' objects to and from EEPROM.
+    template< typename T > T &get( int idx, T &t ){
+        #if defined(__has_include) && __has_include(<type_traits>)
+        static_assert(std::is_trivially_copyable<T>::value,"You can not use this type with EEPROM.get" ); // the code below only makes sense if you can "memcpy" T
+        #endif
+        //EEPtr e = idx;
+        uint8_t *ptr = (uint8_t*) &t;
+        //for( int count = sizeof(T) ; count ; --count, ++e )  *ptr++ = *e;
+        eeprom_read_block(idx, ptr, sizeof(T));
+        return t;
+    }
+
+    template< typename T > const T &put( int idx, const T &t ){
+        #if defined(__has_include) && __has_include(<type_traits>)
+        static_assert(std::is_trivially_copyable<T>::value, "You can not use this type with EEPROM.put"); // the code below only makes sense if you can "memcpy" T
+        #endif
+        uint8_t *ptr =  (uint8_t*) &t;
+        
+        eeprom_write_block(idx, ptr, sizeof(T));
+
+       // EEPtr e = idx;
+        //for( int count = sizeof(T) ; count ; --count, ++e )  (*e).update( *ptr++ );
+
+        return t;
+    }
+};
+
+
+// put - Specialization for Arduino Strings -------------------------------
+// to put an Arduino String to the EEPROM we copy its internal buffer
+// including the trailing \0 to the eprom
+#if defined(ARDUINO_STRING_SUPPORT)
+template <>
+inline const String &EEPROMClass::put(int idx, const String &s)
+{
+    const uint8_t *ptr = (uint8_t *)s.c_str();
+
+#ifdef __arm__
+    eeprom_write_block(ptr, (void *)idx, s.length() + 1); // length() doesn't account for the trailing \0
+#else
+    EEPtr e = idx;
+    for (int count = s.length() + 1; count; --count, ++e)
+        (*e).update(*ptr++);
+#endif
+    return s;
+}
+
+// get - Specialization for Arduino Strings -------------------------------
+// to "get" an Arduino String from the EEPROM we append chars from the EEPROM
+// into it until we find the delimiting /0.
+// String.append is not very efficient, code could probably be opitimized if required...
+
+template <>
+inline String &EEPROMClass::get(int idx, String &s){
+    s = "";             // just in case...
+    EEPtr e = idx;
+
+    char c = *e;        // read in bytes until we find the terminating \0
+    while (c != '\0')
+    {
+        s.append(c);
+        c = *(++e);
+    }
+    return s;
+}
+
+#endif
+
+static EEPROMClass EEPROM __attribute__ ((unused));
+#endif
---
+++ libraries/EEPROM/examples/EEPROM/EEPROM.ino
@@ -0,0 +1,54 @@
+
+#include <Arduino.h>
+#include <EEPROM.h>
+
+EEPROMClass W_EEPROM;           // initialize EEPROM with default size = 2048 bytes
+//EEPROMClass W_EEPROM(4096);   // or you can choose different size
+
+
+// sample data sctructure
+struct data_storage
+{
+  uint32_t offset;
+  uint8_t data1;
+  uint16_t data2;
+  float data3;
+};
+
+// sample data values
+data_storage ds = {
+             .offset = 200,
+             .data1 = 155,
+             .data2 = 0x8000,
+             .data3 = 39.14f
+};
+data_storage ds1;
+uint8_t a1 = 122;
+uint8_t a2 = 255;
+
+void setup() {
+  printf("\nInit EEPROM size = %d bytes\n", W_EEPROM.length());
+
+  // storing data
+  printf("Write to EEPROM:\n");
+  if (! W_EEPROM.write(0, a1))          // store single byte
+     { printf("EEPROM error!\n");}
+
+  W_EEPROM.put(10, ds);                 // store whole structure
+  
+  printf("DS offset = %d, data1 = %d, data2 = %d, data3 = %f\n", ds.offset, ds.data1, ds.data2, ds.data3);
+  delay(500);
+
+  // reading data
+  printf("\nRead from EEPROM:\n");
+  a2 = W_EEPROM.read(0);              // read single byte
+  printf("Byte a2 = %d\n", a2);
+  W_EEPROM.get(10, ds1);              // read structure
+  printf("DS1 offset = %d, data1 = %d, data2 = %d, data3 = %f\n", ds1.offset, ds1.data1, ds1.data2, ds1.data3);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
