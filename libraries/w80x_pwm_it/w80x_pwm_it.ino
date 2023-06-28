9c0e5338834074b929e32e4ba71a88fc4fcb1a4b
board707 <dd@jwee.ru>
2023-06-28 17:35:59 +0300

    Update w80x_pwm_it sketch

--- libraries/w80x_pwm_it/w80x_pwm_it.ino
+++ libraries/w80x_pwm_it/w80x_pwm_it.ino
@@ -4,17 +4,18 @@ Serial.begin(115200);
 pinMode(PB12, PWM_OUT);
 pinMode(LED_BUILTIN_1, OUTPUT);
 digitalWrite(LED_BUILTIN_1, LOW);
-uint32_t freq = setPWMFreq(PB12, 1000);
+PWM_HandleTypeDef *hpwm = getPWMHandle(PB12);
+uint32_t freq = setPWMFreq(hpwm, 1000);
 if (freq > 0) {
   Serial.printf("Set PWM0 freq = %d\n", freq);
   analogWrite(PB12, 64);
-  setPWM_Inverse(PB12, false, false);
+  setPWM_Inverse(hpwm, false, false);
   delay(3000);
   digitalWrite(LED_BUILTIN_1, HIGH);
   delay(1000);
-  setPWM_OneShotMode(PB12, false, 8, true);
+  setPWM_OneShotMode(hpwm, false, 8, true);
   delay(500);
-  setPWM_Inverse(PB12, false, true);
+  setPWM_Inverse(hpwm, false, true);
   
 }
 else { Serial.println("Fail to change frequency of PWM"); }
