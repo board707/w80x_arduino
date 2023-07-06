41613d6fff24751fb92223baa85afb164acf6451
board707 <ddt@jwee.ru>
2023-07-07 01:10:33 +0300

    Add _cxa_pure_virtual func to prevent bug

--- cores/w806/main.cpp
+++ cores/w806/main.cpp
@@ -55,3 +55,5 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
 	printf("Wrong parameters value: file %s on line %d\r\n", file, line);
 }
+
+extern "C" void __cxa_pure_virtual() { while (1); }
