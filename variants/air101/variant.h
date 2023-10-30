9956d8d87eacd5231311dbc7bcad7741844505e9
board707 <50185434+board707@users.noreply.github.com>
2023-10-30 15:18:12 +0300

    Added Air101 board.

---
+++ variants/air101/variant.h
@@ -0,0 +1,18 @@
+#ifndef _VARIANT_ARDUINO_Air101_
+#define _VARIANT_ARDUINO_Air101_
+
+#define LED_BUILTIN_1 PB8
+#define LED_BUILTIN_2 PB9
+#define LED_BUILTIN_3 PB10
+
+// SPI
+#define PIN_SPI_SS    (PB4) 
+#define PIN_SPI_SCK   (PB2)
+#define PIN_SPI_MISO  (PB3)
+#define PIN_SPI_MOSI  (PB5)
+
+#define PINS_COUNT  (44U) // pins in total
+#define ADC_COUNT   (2U)  // number of ADC channels
+#define PWM_COUNT   (5U)  // number of PWM channels
+
+#endif /* _VARIANT_ARDUINO_W801_ */
